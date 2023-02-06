ebf2ea4501a4a11bebb0208464c96255e894547f
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2023-02-06 23:02:46 +0300

    completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+def greet (name)
+    puts "Hello, #{name}!"
+end
+ greet("joanne")
+
+
+ def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+ end
+
+  greet_with_default()
+
+def add(num1, num2)
+    return num1+num2
+end
+add(1,2)
+
+def halve(num)
+    if num.class != Integer
+        return nil
+    end
+
+    return num / 2  
+end
+
